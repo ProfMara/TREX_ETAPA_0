04561d4471f4c00565c2fa8c1c8e969b006bc3c9
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-02-13 13:45:55 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+//PASSO 1 CRIAR AS VARIÁVEIS
+var trex, trex_correndo;
+var solo, soloimagem;
+
+function preload() {
+    //carregar a imagem do solo
+
+    //carregar a animação do t-rex
+
+}
+
+function setup() {
+    createCanvas(600, 200);
+
+    //criar a sprite solo;
+
+    //adicionar a imagem na sprite solo
+
+    //dar velocidade ao solo
+
+
+
+
+    //criar a sprite trex;
+
+    //adicionar a animação na sprite do t-rex
+
+    //definir o tamanho da animação
+
+
+}
+
+function draw() {
+    background("white");
+
+    //reiniciar a posição do solo automaticamente
+
+
+    //fazer o trex pular se apertar espaço
+
+
+    //dar gravidade ao trex
+
+
+    //impedir o trex de cair no abismo
+
+
+    drawSprites();
+
+}
